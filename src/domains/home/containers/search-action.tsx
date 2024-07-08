ea78845f5143ee4ccd4d81f871268436385090bb
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-07-08 22:27:56 +0330

    refactor: spacing in search action and category

--- src/domains/home/containers/search-action.tsx
+++ src/domains/home/containers/search-action.tsx
@@ -4,18 +4,21 @@ import { Button } from '@/shared/ui';
 
 export function SearchAction() {
   return (
-    <section>
-      <div className="my-2 flex items-center justify-between rounded-[12px] bg-surface-secondary p-2.5">
-        <Link href="/search" className="flex gap-2.5">
+    <section id="search-action" data-testid="search-action">
+      <Link
+        href="/search"
+        className="my-2 flex items-center justify-between rounded-[12px] bg-surface-tertiary p-2.5"
+      >
+        <div className="flex gap-2.5">
           <Icon name="magnifier" iconClassName="text-content-secondary" />
-          <p className="text-base font-medium text-content-tertiary">
+          <p className="text-labelMedium text-content-tertiary">
             جستجوی محصول...
           </p>
-        </Link>
+        </div>
         <Button variant="ghost">
           <Icon iconClassName="text-content-primary" name="microphone" />
         </Button>
-      </div>
+      </Link>
     </section>
   );
 }
