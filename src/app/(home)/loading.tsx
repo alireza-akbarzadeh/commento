5fa591415afea5541ba54bd5a266df11923402b9
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-07-18 17:53:01 +0330

    refactor: update loading components

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -2,8 +2,8 @@ import { LoadingDots } from '@/shared/components';
 
 const Loading = () => {
   return (
-    <div className="w-full max-w-sm">
-      <div className="flex h-dvh items-center justify-center">
+    <div className="flex h-dvh items-center justify-center">
+      <div className="w-[384px] bg-white">
         <LoadingDots />
       </div>
     </div>
--- src/shared/components/loading/dots.tsx
+++ src/shared/components/loading/dots.tsx
@@ -1,6 +1,6 @@
 export function LoadingDots() {
   return (
-    <div className="flex h-screen items-center justify-center space-x-2 bg-white dark:invert">
+    <div className="flex h-screen items-center justify-center space-x-2 dark:invert">
       <span className="sr-only">Loading...</span>
       <div className="size-4 animate-bounce rounded-full bg-content-primary [animation-delay:-0.3s]"></div>
       <div className="size-4 animate-bounce rounded-full bg-content-primary [animation-delay:-0.15s]"></div>
