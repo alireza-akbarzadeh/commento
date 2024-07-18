25d92e6aec5c44d8643aec8b2c5a8a8493535137
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-07-18 14:46:14 +0330

    refactor: spacing issue in compration section

--- src/domains/home/containers/comperation/comparetaion-item.tsx
+++ src/domains/home/containers/comperation/comparetaion-item.tsx
@@ -18,75 +18,19 @@ export function ComparetaionItem(props: ComparetaionItemProps) {
     <CarouselItem className="basis-[93%] pt-3">
       <Card className="relative mb-3 rounded-2xl">
         <CardContent className="flex gap-1.5 px-4 py-2">
-          <div className="flex flex-col">
-            <Link
-              href={`/product/${product.title}`}
-              className="flex-center relative m-2.5 flex-col"
-            >
-              <div className="flex size-[125px] items-center justify-center rounded-lg bg-surface-tertiary">
-                <div className="relative h-[101px] w-[99px]">
-                  <LazyImage
-                    src={product.imageUrl}
-                    style={{
-                      objectFit: 'cover',
-                      position: 'absolute',
-                    }}
-                    isLocal
-                    quality={100}
-                    alt="banner"
-                    fill
-                  />
-                </div>
-              </div>
-              {product.discount && (
-                <DiscountBadge discount={product.discount} />
-              )}
-            </Link>
-            <div className="space-y-1 pr-2 pt-3">
-              <p className="text-labelSmall text-content-primary">
-                {product.title.length > 40
-                  ? product.title.slice(0, 43).concat('...')
-                  : product.title}
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <Link
-              href={`/product/${product.title}`}
-              className="flex-center relative m-2.5 flex-col"
-            >
-              <div className="flex-center size-[125px] rounded-[8px] bg-surface-tertiary">
-                <div className="relative h-[101px] w-[99px]">
-                  <LazyImage
-                    src={product.imageUrl}
-                    isLocal
-                    quality={100}
-                    alt="banner"
-                    fill
-                  />
-                </div>
-              </div>
-              {product.discount && (
-                <DiscountBadge discount={product.discount} />
-              )}
-            </Link>
-            <div className="pr -2 space-y-1 pt-3">
-              <p className="text-labelSmall text-content-primary">
-                {product.title.length > 40
-                  ? product.title.slice(0, 43).concat('...')
-                  : product.title}
-              </p>
-            </div>
-          </div>
+          <Item {...product} />
+          <Item {...product} />
         </CardContent>
-        <Separator className="border-1 mx-auto my-4 mb-3 w-[86%] border-t border-border-primary" />
-        <CardFooter className="py-4">
-          <div className="flex w-full items-center justify-between">
-            <p className="text-labelMedium text-content-primary">
-              مشاهده مقایسه
-            </p>
-            <div className="flex items-center gap-[7.5px]">
-              <span className="text-labelMedium text-content-tertiary">۲</span>
+        <Separator className="border-1 mx-auto mt-2 w-[90%] border-t border-border-primary" />
+        <CardFooter className="flex w-full items-center justify-between py-3">
+          <p className="text-labelMedium text-content-primary">مشاهده مقایسه</p>
+          <div className="flex items-center">
+            <div className="flex-center size-6 rounded-full bg-surface-secondary">
+              <span className="pl-[3px] pt-[3px] text-labelMedium text-content-tertiary">
+                ۲
+              </span>
+            </div>
+            <div className="size-6">
               <Icon
                 iconClassName="text-content-tertiary"
                 name="chevron-left"
@@ -97,5 +41,28 @@ export function ComparetaionItem(props: ComparetaionItemProps) {
         </CardFooter>
       </Card>
     </CarouselItem>
+  );
+}
+
+function Item({ title, imageUrl, discount }: Products) {
+  return (
+    <div className="flex flex-col">
+      <Link
+        href={`/product/${title}`}
+        className="flex-center relative m-2.5 flex-col"
+      >
+        <div className="flex-center size-[125px] rounded-[8px] bg-surface-tertiary">
+          <div className="relative h-[101px] w-[99px]">
+            <LazyImage src={imageUrl} isLocal quality={100} alt="banner" fill />
+          </div>
+        </div>
+        {discount && <DiscountBadge discount={discount} />}
+      </Link>
+      <div className="pr -2 space-y-1 pt-3">
+        <p className="text-labelSmall text-content-primary">
+          {title.length > 40 ? title.slice(0, 38).concat('...') : title}
+        </p>
+      </div>
+    </div>
   );
 }
