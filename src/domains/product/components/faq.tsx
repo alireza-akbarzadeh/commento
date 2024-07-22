ba8b9bf5edc8b2b7af30eed2a1cab1529d69b6f3
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-07-22 22:21:51 +0330

    feat: change faq section bg

--- src/domains/product/components/faq.tsx
+++ src/domains/product/components/faq.tsx
@@ -3,8 +3,8 @@ import { Icon } from '@/shared/components';
 
 export function FAQ() {
   return (
-    <div className="mb-2 mt-4 bg-surface-secondary py-2">
-      <div className="flex w-full items-center justify-between bg-content-inverse px-4">
+    <div className="mb-2 mt-4 bg-content-inverse py-2">
+      <div className="flex w-full items-center justify-between px-4">
         <div className="flex items-center gap-[6px]">
           <p className="text-content-primary text-labellarge">پرسش و پاسخ‌ها</p>
           <Icon size="small" name="chevron-left" />
