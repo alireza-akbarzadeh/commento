1c4ee698d223e8b6192e5ace58bf0b4676787bef
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-08-10 21:24:06 +0330

    feat: increase store card  bottom sheet

--- src/domains/stores/components/bottom-sheet-store.tsx
+++ src/domains/stores/components/bottom-sheet-store.tsx
@@ -3,49 +3,59 @@ import { useState } from 'react';
 import { Icon, StoreCard } from '@/shared/components';
 import { useScroll } from '@/shared/hooks';
 import { Button, Drawer, DrawerContent } from '@/shared/ui';
+import { cn } from '@/shared/utils';
 
 type BottomSheetStoreType = {
-    showStore: boolean;
-    handleToggleClick: () => void
+  showStore: boolean;
+  handleToggleClick: () => void;
 };
 
 export function BottomSheetStore(props: BottomSheetStoreType) {
-    const { showStore, handleToggleClick } = props;
-    const isSticky = useScroll(500);
-    const [activeSnapPoint, setActiveSnapPoint] = useState<number | string | null>(1); // Default to full height
+  const { showStore, handleToggleClick } = props;
+  //Fixme: check why scrolling is not working
+  const isSticky = useScroll(60);
+  const [activeSnapPoint, setActiveSnapPoint] = useState<
+    number | string | null
+  >(0);
 
-    const handleDrag = (_event: React.PointerEvent<HTMLDivElement>, percentageDragged: number) => {
-        console.log({ percentageDragged });
+  const handleDrag = (
+    _event: React.PointerEvent<HTMLDivElement>,
+    percentageDragged: number,
+  ) => {
+    if (percentageDragged > 0.0033) {
+      setActiveSnapPoint(1);
+    }
+  };
+  const numberPfStoreList = activeSnapPoint === 0 ? 1 : 5;
 
-        // If dragged more than 33% up the screen, snap to 2/3 height
-        if (percentageDragged > 0.33 && activeSnapPoint !== 0.67) {
-            setActiveSnapPoint(0.67);
-        } else if (percentageDragged <= 0.33 && activeSnapPoint !== 1) {
-            setActiveSnapPoint(1); // Full height
-        }
-    };
+  console.log({ isSticky });
 
-    return (
-        <Drawer
-            open={showStore}
-            onClose={handleToggleClick}
-            modal={false}
-        >
-            <DrawerContent
-                noOverlay
-                className={`${showStore ? "mx-4 sm:m-auto" : ""} ${'h-4/5'} `}
-            >
-                <StoreCard open navigation="/stores/1" />
-                {isSticky && (
-                    <Button
-                        fullWidth
-                        className="fixed bottom-10 left-1/2 flex h-14 -translate-x-1/2 items-center gap-2 rounded-2xl"
-                    >
-                        <Icon iconClassName="text-content-inverse" name="map-fill" />
-                        <span>فروشگاه‌ها روی نقشه</span>
-                    </Button>
-                )}
-            </DrawerContent>
-        </Drawer>
-    );
+  return (
+    <Drawer
+      open={showStore}
+      onClose={handleToggleClick}
+      onDrag={handleDrag}
+      modal={false}
+    >
+      <DrawerContent
+        noOverlay
+        className={cn('m-auto h-auto', {
+          'm-auto h-4/5': activeSnapPoint === 1,
+          'mx-4 sm:m-auto': activeSnapPoint !== 1 && showStore,
+        })}
+      >
+        <div className="overflow-x-auto">
+          {Array.from({ length: numberPfStoreList }).map((_, index) => (
+            <StoreCard key={index} open navigation="/stores/1" />
+          ))}
+        </div>
+        {isSticky && (
+          <Button className="fixed bottom-10 left-1/2 flex h-14 -translate-x-1/2 items-center gap-2 rounded-2xl">
+            <Icon iconClassName="text-content-inverse" name="map-fill" />
+            <span>فروشگاه‌ها روی نقشه</span>
+          </Button>
+        )}
+      </DrawerContent>
+    </Drawer>
+  );
 }
